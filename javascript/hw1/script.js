950da5ec89c85fa4bff071728111c8d558b38b95
Slava Tuliakov <slava.tuliakov@gmail.com>
2020-03-19 19:16:44 +0200

    js-hw1

---
+++ javascript/hw1/script.js
@@ -0,0 +1,21 @@
+let name = '';
+do {
+	name = prompt('Please, enter your name', [ name ]);
+} while (name == '');
+
+let age = '';
+while (age == '' || isNaN(age)) {
+	age = prompt('Please, enter your age', [ age ]);
+}
+
+if (age > 22) {
+	alert('Welcome, ' + name);
+} else if (age > 18) {
+	if (confirm('Are you sure you want to continue?')) {
+		alert('Welcome, ' + name);
+	} else {
+		alert('You are not allowed to visit this website.');
+	}
+} else {
+	alert('You are not allowed to visit this website.');
+}
