4cce7c568f441d97851ac929f5ac2a17fefc624c
Slava Tuliakov <slava.tuliakov@gmail.com>
2020-08-28 10:55:53 +0300

    hw4

---
+++ advanceed_javascript/hw4/script.js
@@ -0,0 +1,27 @@
+const button = document.querySelector('.button');
+button.onclick = () => {
+	console.log('click');
+	fetch('https://api.ipify.org/?format=json').then((response) => response.json()).then((json) => {
+		findByIp(...Object.values(json));
+	});
+};
+
+function findByIp(ip) {
+	fetch(`http://ip-api.com/json/${ip}?fields=message,continent,country,regionName,city,district`)
+		.then((response) => response.json())
+		.then((json) => renderData(Object.keys(json), Object.values(json), json));
+}
+
+function renderData(keys, values, object) {
+	const ul = document.createElement('ul');
+
+	for (const key in object) {
+		const li = document.createElement('li');
+
+			object[key] === '' ? (li.innerText = `${key}: field is empty`) :
+			(li.innerText = `${key}: ${object[key]}`);
+
+		ul.appendChild(li);
+	}
+	document.body.appendChild(ul);
+}
